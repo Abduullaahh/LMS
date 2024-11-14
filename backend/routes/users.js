9179da24650500c7c45915afc699fadd942becb6
Abduullaahh <hafizabdullahtahir@gmail.com>
2024-11-15 03:59:40 +0500

    Backend completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -20,8 +20,7 @@ router.get('/:id', (req, res) => {
 });
 
 // Get user by role
-router.get('/role/:role', (req, res) => {
-    console.log('i was here')
+router.get('/:role', (req, res) => {
     const { role } = req.params;
     connection.query('SELECT * FROM Users WHERE role = ?', [role], (err, result) => {
         if (err) return res.status(500).json({ error: err.message });
